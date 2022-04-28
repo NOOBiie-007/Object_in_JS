d5e132fc0e0709c0c8e41a5d9a78eae90cd594d7
NOOBiie-007 <77447769+NOOBiie-007@users.noreply.github.com>
2022-04-28 23:32:37 +0600

    Object in Javascript

---
+++ object.js
@@ -0,0 +1,15 @@
+// Set of Property//
+var nkb ={name:'Nakib', age: 22, FFood:'Cake'}
+console.log(nkb);
+
+// Show specific Property
+var m = nkb.FFood;
+console.log(m);
+
+// Update Some property
+nkb.age=23;
+console.log(nkb);
+
+//Add new property
+nkb.FMovie='Avenger:End Game';
+console.log(nkb.FMovie);
